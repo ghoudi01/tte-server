28ed18d63a4a3f971b6ce5cfc96785c9719bbb1e
ghoudi01 <ghoudi31@gmail.com>
2026-04-20 20:20:22 +0100

    f

--- config/plugins.ts
+++ config/plugins.ts
@@ -3,6 +3,7 @@ import type { Core } from '@strapi/strapi';
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
   'users-permissions': {
     config: {
+      jwtSecret: env('JWT_SECRET', 'dev-jwt-secret-change-me'),
       jwt: {
         expiresIn: '7d',
       },
